024250dbcf5d61ed5d28912ad823b335fe937ec4
esmagelebek <esgelebek@gmail.com>
2023-02-24 15:45:24 +0300

    24.02.23

--- TemelVeriTurleri/TemelVeriTurleri.cpp
+++ TemelVeriTurleri/TemelVeriTurleri.cpp
@@ -5,6 +5,7 @@
 
 int main()
 {
+    /*
     char d = 'A';
     char a = 10;
     char e = 10;
@@ -28,6 +29,31 @@ int main()
     std::cout << "x yeni deger:" << x << '\n';
     x = 65537;
     std::cout << "x son deger:" << x << '\n';
+    */
+
+    /*
+    unsigned int x{3};
+    unsigned int y{ 5 };
+
+    std::cout << x-y << '\n';
+    */
+
+    double f{123456789.0};
+
+    std::cout << std::setprecision(6);
+    std::cout << f << '\n';
+
+    //std::cout << std::setprecision(16);
+    // std::cout << 9876543.21 << '\n';
+
+    //int8_t d = 115;
+
+    //std::cout << d << '\n';
+
+
+
+
+
 
 
 
